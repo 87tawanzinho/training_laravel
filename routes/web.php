7c1dc6baf5ec146b7cc5246ece7c376743219501
tawan <tawan@tecnovix.com>
2024-12-05 16:17:34 -0300

    origin

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\ProdutoController;
+use Illuminate\Support\Facades\Route;
+
+
+
+
+Route::get('/', [ProdutoController::class, 'index']);
+
+Route::get('/produto/{id?}', [ProdutoController::class, 'show'])-> name('produto.name');
